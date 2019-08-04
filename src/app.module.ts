c93c8d1eadab3469fbd1763ad479aab883a4a037
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-04 22:01:00 +0900

    Tasks Moduleの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './db.config';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [TasksModule],
 })
 export class AppModule {}
